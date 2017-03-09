6265322454f28056323aa260eb065960ad5a0e67
Craig Rodrigues <PagingCraig@gmail.com>
2017-03-08 19:40:20 -0800

    Add grunt concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,16 @@ module.exports = function(grunt) {
       }
     },
 
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['src/intro.js', 'src/project.js', 'src/outro.js'],
+        dest: 'dist/built.js',
+      },
+    },
+
     uglify: {
     },
 
@@ -63,6 +73,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.registerTask('server-dev', function (target) {
     grunt.task.run([ 'nodemon', 'watch' ]);
